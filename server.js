7ca10337fc17b2400ded1fc5baa19975cfa19ac6
DESKTOP-CNSKD0H\rotem <rotem@feedbacktuner.com>
2019-05-17 13:29:58 +0300

    first step

--- server.js
+++ server.js
@@ -1,29 +1,23 @@
-process.env['PORT'] = process.env['PORT']  || '8080';
+process.env['PORT'] = process.env['PORT'] || '3000';
 process.env['DEBUG_PORT'] = process.env['DEBUG_PORT'] || '9229';
-process.env['DS_API_HOST'] = process.env['DS_API_HOST']  || 'http://eng-task.pxchk.net';
 process.env['CACHE_TTL_IN_SECS'] = process.env['CACHE_TTL_IN_SECS'] || '3600';
 
 const express = require('express');
 const app = express();
-const { cache, exceptionHandler, botDetect } = require('./src/middlewares');
-const cookieParser = require('cookie-parser');
-const router = require('./src/router/routes');
+const packageRouter = require('./src/router/package');
+const appRouter = require('./src/router/app');
 const ejs = require('ejs');
-
 const path = require('path');
-const { PORT = 8080 } = process.env;
- 
+const { PORT = 3000 } = process.env;
+
 app.engine('html', ejs.renderFile);
 app.set('view engine', 'html');
 app.set("views", path.join(__dirname, 'src', "views"));
-app.use(cookieParser());
-app.use(cache);
-app.use(botDetect);
-app.use(exceptionHandler);
-app.use(router);
-
-const server = app.listen(PORT, () => {
-    console.log(`Express running → PORT ${ server.address().port }`);
+app.use('/package', packageRouter)
+app.use('/app', appRouter)
+ 
+const server = app.listen(PORT, async() => {
+   console.log(`Express running → PORT ${ server.address().port }`);
 });
 
 module.exports = app;
---
+++ src/crawler.js
@@ -0,0 +1,92 @@
+const Axios = require('axios');
+const axios = Axios.create({
+    baseURL: 'https://registry.npmjs.org/',
+    timeout: 10000,
+});
+const fs = require('fs');
+const _ = require('lodash');
+const Path = require('path');
+const util = require('util');
+
+const normalizeVersion = (version) => {
+    let exp = version.charAt(0) === '~' || version.charAt(0) === '^';
+    version = exp ? version.slice(1, version.length) : version;
+    version = version.replace("*", "latest");
+    if (version.length == 1) return `${version}.0.0`;
+    return version;
+}
+const getUrl = (packageName, packageVersion) => `${packageName}/${normalizeVersion(packageVersion)}`;
+const xmlbuilder = require('xmlbuilder');
+
+let queue = [];
+let cache = {};
+
+const getData = async (package) => {
+    let dep = {};
+    let url = getUrl(package.name, package.version);
+    try {
+        var {
+            data: {
+                dependencies,
+                devDependencies
+            }
+        } = await axios.get(url);
+        dep = _.merge(dependencies, {}); //devDependencies
+    } catch (e) {
+        console.log(`${url}, failed fatching package name: ${package.name}, version: ${package.version}`);
+    }
+    return dep;
+}
+
+module.exports = async (package) => {
+    let fileName = Path.join(process.cwd(), "temp", `${Date.now()}.xml`);
+    let writeStream = fs.createWriteStream(fileName, { flags : 'w', encoding: 'utf-8' });
+    let writer = xmlbuilder.streamWriter(writeStream, { pretty: true, width: 20 }); // pass a writable stream
+    const done = util.promisify(writer.stream.on).bind(writer);
+    let root = xmlbuilder.create('root'); // build the XML document
+    let circuler = {};
+    let dep = await getData(package);
+    _.each(dep, (value, key) => {
+        var node = {
+            name: key,
+            version: normalizeVersion(value),
+            url: getUrl(key, value),
+            dep: getData({
+                name: key,
+                version: value
+            }),
+            parent: root,
+            height: 0
+        };
+        queue.push(node);
+    });
+    while (queue.length > 0) {
+        var package = queue.pop();
+        let cachedVersion = cache[package.name];
+        var parent = package.parent;
+        var child = parent.ele('package', {
+            'name': package.name,
+            'version': package.version
+        });
+        if (circuler[package.name]) continue;
+        dep = cachedVersion || await package.dep;
+        cache[package.name] = dep;
+        circuler[package.name] = true;
+        _.each(dep, (value, key) => {
+            queue.push({
+                name: key,
+                version: value,
+                url: getUrl(key, value),
+                dep: getData({
+                    name: key,
+                    version: value
+                }),
+                parent: child,
+                height: package.height++
+            });
+        });
+    }
+    root.end(writer); // call end to pass the XML document to the stream writer
+    var finish = await done('finish');
+    return fileName;
+};
