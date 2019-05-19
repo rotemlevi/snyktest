a468f8cdbeb839ca9964c1a214317c033d1ef64c
DESKTOP-CNSKD0H\rotem <rotem@feedbacktuner.com>
2019-05-19 21:55:44 +0300

    ready for production

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,39 +3,31 @@ const semver = require('semver')
 const _ = require('lodash');
 const fs = require('fs');
 const util = require('util');
- 
+
 const normalizeVersion = (package) => {
-    let cleanVersion = package.version.toLowerCase();
-    try {
-        if (package.name.charAt(0) === '@') return "*";  //scoped packages fix
-        if (cleanVersion === "latest") return "latest";
-        return semver.coerce(cleanVersion).raw;
-    } catch (e) {
-        return cleanVersion;
-    }
+    let cleanVersion = package.version; //.toLowerCase().replace("*", "latest");
+    if (package.name.charAt(0) === '@') return "*";
+    if (cleanVersion === "latest") return "latest";
+    let semverObject = semver.coerce(cleanVersion);
+    return semverObject ? semverObject.raw : cleanVersion;
 };
-const getUrl = (package) => `${encodeURIComponent(package.name)}/${normalizeVersion(package)}`;
-const normalizeName = (name) => {
-    var temp = name.split('/');
-    if (temp.length > 0) return temp[0];
-    return temp;
-};
+
+const getUrl = (package) => `${normalizeName(package.name)}/${normalizeVersion(package)}`;
+const normalizeName = (name) => encodeURIComponent(name);
 const generateGraphPackageKey = (package) => crypto.createHash('md5').update(`${package.name}:${package.version}`).digest('hex');
 
 module.exports = {
-    deleteFileAsyc: (file) => {
-        return util.promisify(fs.unlink).bind(fs)(file);
-    },
     normalizeVersion: normalizeVersion,
     getUrl: getUrl,
     normalizeName: normalizeName,
+    generateGraphPackageKey: generateGraphPackageKey,
+    deleteFileAsyc: (file) => util.promisify(fs.unlink).bind(fs)(file),
     delay: (time) => (result) => new Promise(resolve => setTimeout(() => resolve(result), time)),
     validateDep: (dep) => Array.isArray(dep),
     xmlWriterDone: (writeStream, cb) => new Promise((resolve, reject) => {
         writeStream.on('finish', () => resolve("finished"));
         writeStream.on('error', (err) => reject(err));
     }),
-    generateGraphPackageKey: generateGraphPackageKey,
     generateGraphPackage: (package, parentPackage) => {
         let packageKey = generateGraphPackageKey(package);
         let parentPackageKey = !_.isEmpty(parentPackage) ?
